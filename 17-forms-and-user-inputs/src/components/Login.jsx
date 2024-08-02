4a47f9a8f345d11f8cb89a3fdc1a31db45ae6df4
Gabe Richard <gabe_rch@hotmail.com>
2024-08-02 10:19:17 -0400

    End of lecture 260

--- 17-forms-and-user-inputs/src/components/Login.jsx
+++ 17-forms-and-user-inputs/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
 
   const email = useRef();
   const password = useRef();
@@ -10,6 +12,15 @@ export default function Login() {
     
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
 
     console.log(enteredEmail, enteredPassword);
   }
@@ -27,6 +38,7 @@ export default function Login() {
             name="email" 
             ref={email}
           />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
--- 17-forms-and-user-inputs/src/components/StateLogin.jsx
+++ 17-forms-and-user-inputs/src/components/StateLogin.jsx
@@ -7,6 +7,11 @@ export default function Login() {
     email: '',
     password: ''
   });
+
+  const [didEdit, setDidEdit] = useState ({
+    email: false,
+    password: false
+  })
   
   function handleSubmit(event) {
     event.preventDefault();
@@ -22,11 +27,24 @@ export default function Login() {
     setEnteredPassword(event.target.value);
   }
 
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+
   function handleInputChange(identifier, value) {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value
     }));
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: false
+    }))
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true
+    }))
   }
 
   return (
@@ -42,7 +60,9 @@ export default function Login() {
             name="email" 
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
+            onBlur={() => handleInputBlur('email')}
           />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
